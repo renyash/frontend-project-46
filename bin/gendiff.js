4a2a04cdddd27644fd43221d5b90b323293f85f3
nikita.b <bulatov.nre@gmail.com>
2023-04-18 22:00:15 +0300

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/gendiff.js';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows difference.')
     .version('0.1.0')
+    .description('Compares two configuration files and shows difference.')
+    .option('-f, --format <type>', 'output format')
     .argument('<filepath1>')
     .argument('<filepath2>')
-    .option('-f, --format <type>', 'output format');
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(filepath1, filepath2));
+    });
 
 program.parse();
---
+++ src/gendiff.js
@@ -0,0 +1,57 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const getDataFile1 = readFileSync(path.resolve('__fixtures__', 'file1.json'), 'utf8');
+const getDataFile2 = readFileSync(path.resolve('__fixtures__', 'file2.json'), 'utf8');
+
+console.log('reading file1', getDataFile1);
+console.log('reading file2', getDataFile2);
+
+const mknode = (key, value, type, oldValue = null) => ({
+    key,
+    value,
+    type,
+    oldValue
+});
+
+const parsedDataFile1 = JSON.parse(getDataFile1);
+const parsedDataFile2 = JSON.parse(getDataFile2);
+
+const getDiff = (parsedDataFile1, parsedDataFile2) => {
+const keys1 = Object.keys(parsedDataFile1);
+const keys2 = Object.keys(parsedDataFile2);
+
+console.log(`keysFile1 ${keys1}`);
+console.log(`keysFile2 ${keys2}`);
+    const allKeys = _.union(keys1, keys2);
+    console.log(`Here all unique keys found: ${allKeys}`);
+    const nodes = allKeys.map((key) => {
+        const value1 = parsedDataFile1[key];
+        const value2 = parsedDataFile2[key];
+        if (!_.has(parsedDataFile1, key)) return mknode(key, value2, 'added');
+        if (!_.has(parsedDataFile2, key)) return mknode(key, value1, 'deleted');
+        if (parsedDataFile1[key] !== parsedDataFile2[key]) return mknode (key, value1, 'updated', value2);
+        return mknode(key, value1, 'unchanged');
+    });
+    return nodes;
+};
+
+const renderTree = (nodes) => {
+    const tree = nodes.map((node) => {
+        const {key, value, oldValue} = node;
+        switch(node.type) {
+            case 'added': return `\t+ ${key}: ${value}`;
+            case 'deleted': return `\t- ${key}: ${value}`;
+            case 'updated': return `\t- ${key}: ${oldValue}\n\t+ ${key}: ${value}`;
+            case 'unchanged': return `\t  ${key}: ${value}`;
+            default: `something went wrong... :(`;
+        };
+    });
+    return `{\n${tree.join('\n')} \n}`;
+    
+}
+const nodes = getDiff(parsedDataFile1, parsedDataFile2);
+console.log(renderTree(nodes));
+
+export default getDiff;
