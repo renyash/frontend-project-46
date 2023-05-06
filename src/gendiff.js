c9c307e8925d2c306e6861c13862c690dbfe65d8
Bulatov N <bulatov.nre@gmail.com>
2023-05-06 15:14:33 +0300

    sorting added

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,6 +5,9 @@ import _ from 'lodash';
 const getDataFile1 = readFileSync(path.resolve('__fixtures__', 'file1.json'), 'utf8');
 const getDataFile2 = readFileSync(path.resolve('__fixtures__', 'file2.json'), 'utf8');
 
+const parsedDataFile1 = JSON.parse(getDataFile1);
+const parsedDataFile2 = JSON.parse(getDataFile2);
+
 const mknode = (key, value, type, oldValue = null) => ({
   key,
   value,
@@ -12,16 +15,17 @@ const mknode = (key, value, type, oldValue = null) => ({
   oldValue,
 });
 
-const parsedDataFile1 = JSON.parse(getDataFile1);
-const parsedDataFile2 = JSON.parse(getDataFile2);
-
 const getDiff = (parsedDataFile1, parsedDataFile2) => {
   const keys1 = Object.keys(parsedDataFile1);
   const keys2 = Object.keys(parsedDataFile2);
 
-  const allKeys = _.union(keys1, keys2);
-  console.log(`Here all unique keys found: ${allKeys}`);
-  const nodes = allKeys.map((key) => {
+  const unitedKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(unitedKeys);
+  
+  console.log(`Here all unique keys found: ${unitedKeys}`);
+  console.log(`Sorted keys: ${sortedKeys}`);
+
+  const nodes = sortedKeys.map((key) => {
     const value1 = parsedDataFile1[key];
     const value2 = parsedDataFile2[key];
     if (!_.has(parsedDataFile1, key)) return mknode(key, value2, 'added');
@@ -40,7 +44,7 @@ const renderTree = (nodes) => {
       case 'deleted': return `\t- ${key}: ${value}`;
       case 'updated': return `\t- ${key}: ${oldValue}\n\t+ ${key}: ${value}`;
       case 'unchanged': return `\t  ${key}: ${value}`;
-      default: `something went wrong... :(`;
+      default: throw new Error('something went wrong... :(');
     }
   });
   return `{\n${tree.join('\n')} \n}`;
