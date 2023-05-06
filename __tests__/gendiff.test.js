d45362cb1fd0c2a0df642c6a3c0b86005d5eed09
Bulatov N <bulatov.nre@gmail.com>
2023-05-06 17:49:33 +0300

    workflow test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,8 @@
-import genDiff from "../src/gendiff.js";
-import path from 'path';
+import genDiff from '../src/gendiff.js';
 
 const testFile1 = './__fixtures__/file1.json';
 const testFile2 = './__fixtures__/file2.json';
 
 test('genDiff tests', () => {
-    expect(genDiff(testFile1, testFile2)).toEqual(genDiff(testFile1, testFile2));
-})
+  expect(genDiff(testFile1, testFile2)).toEqual(genDiff(testFile1, testFile2));
+});
