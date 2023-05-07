d0060da5344de442917c22af142b4f1d0ee667eb
Bulatov N <bulatov.nre@gmail.com>
2023-05-07 22:58:02 +0300

    updating tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,16 @@
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import path, { dirname } from 'path';
 import genDiff from '../src/gendiff.js';
 
-const testFile1 = './__fixtures__/file1.json';
-const testFile2 = './__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const correctTestFile = readFile('testFile.txt');
 
 test('genDiff tests', () => {
-  expect(genDiff(testFile1, testFile2)).toEqual(genDiff(testFile1, testFile2));
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(correctTestFile);
 });
