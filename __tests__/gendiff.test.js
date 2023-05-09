ef0a2822075977e456292772e5d4137eb5d13a3a
nikita.b <bulatov.nre@gmail.com>
2023-05-09 19:16:11 +0300

    fix test files and output

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const correctTestFile = readFile('testFile.txt');
+const correctTestFile = readFile('test_json.txt');
 
 test('genDiff tests', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(correctTestFile);
